78fb4cb654328d094737d15f6793ce3da407ee7e
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-11-13 20:20:39 -0300

    testando persistencia com o banco

---
+++ src/test/java/spring/data/persistence/AppSpringDataTeste.java
@@ -0,0 +1,16 @@
+package spring.data.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+public class AppSpringDataTeste {
+	
+	@Test
+	public void testeInsert() {
+		System.out.println("Iniciou com sucesso");
+	}
+}
