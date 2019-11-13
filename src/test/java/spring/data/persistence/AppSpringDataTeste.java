a9e067a270fb52ed53856dbaea391009ea8de7d9
Jefferson Brandao <jeffersonbraster@gmail.com>
2019-11-13 20:49:51 -0300

    inserindo dados

--- src/test/java/spring/data/persistence/AppSpringDataTeste.java
+++ src/test/java/spring/data/persistence/AppSpringDataTeste.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import dao.InterfaceSpringData;
+import model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
@@ -17,6 +18,17 @@ public class AppSpringDataTeste {
 	
 	@Test
 	public void testeInsert() {
+		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
+		usuarioSpringData.setNome("jeje");
+		usuarioSpringData.setLogin("jejezinho");
+		usuarioSpringData.setSenha("liana123");
+		
+		interfaceSpringData.save(usuarioSpringData);
+	}
+	
+	
+	@Test
+	public void teste() {
 		System.out.println("Iniciou com sucesso");
 	}
 }
